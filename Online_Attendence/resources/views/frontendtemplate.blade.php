c6ad912426d22aabcb33431da749a6195848c9e1
hareom284 <zawzawwin@ucsloikaw.edu.mm>
2020-11-19 01:37:19 +0630

    added footer design

--- Online_Attendence/resources/views/frontendtemplate.blade.php
+++ Online_Attendence/resources/views/frontendtemplate.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Student Attendance</title>
   <link rel="stylesheet" href="{{asset('my_asset/bootstrap/css/bootstrap.min.css')}}">
+  
   <link rel="stylesheet" href="{{asset('my_asset/css/style.css')}}">
   {{-- footer style  --}}
   <style>
