68fea619f9772d87deebaa59822f62422bdd210d
hareom284 <zawzawwin@ucsloikaw.edu.mm>
2020-11-19 14:18:28 +0630

    update attendance design

--- Online_Attendence/resources/views/auth/login.blade.php
+++ Online_Attendence/resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
-
+{{-- @extends('layouts.app') --}}
+@extends('usertemplate')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-sm-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
